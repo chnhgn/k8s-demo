a24394d049fb8ad538668812791015a290c9e69e
Eddy Hu <guoning.hu@sas.com>
2019-11-06 10:24:28 +0800

    Add endpoint

---
+++ Endpoint.py
@@ -0,0 +1,11 @@
+from flask import Flask, json
+
+api = Flask(__name__)
+
+@api.route('/demo', methods=['GET'])
+def get_demo():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    api.run()
